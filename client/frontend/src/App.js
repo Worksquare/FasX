d9d5d47409c4ab050875408fc208804da065e263
folarin <folarintaiye@gmail.com>
2023-07-28 03:21:18 +0100

    work on signup component

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import PasswordReset from "./components/Password reset/PasswordReset";
 import PasswordResetSuccess from "./components/password reset success/PasswordResetSuccess";
-import VerificationSuccess from "./components/verification success/VerificationSuccess";
+// import VerificationSuccess from "./components/verification success/VerificationSuccess";
 // import UserSignup from "./components/user signup/UserSignup";
 import Verification from "./components/verification/Verification";
 
@@ -15,7 +15,7 @@ function App() {
         <PasswordResetSuccess />
         {/* <UserSignup /> */}
         <Verification />
-        <VerificationSuccess />
+        {/* <VerificationSuccess /> */}
       </header>
     </div>
   );
