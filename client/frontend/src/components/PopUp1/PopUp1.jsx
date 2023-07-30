621a6572daf0746bc80f9a38c7d5fa11880cc327
abdulgafar01 <abdulgafarbusari49@gmail.com>
2023-07-30 12:30:49 +0100

    done with popup 1

--- client/frontend/src/components/PopUp1/PopUp1.jsx
+++ client/frontend/src/components/PopUp1/PopUp1.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
+
 import './PopUp1.css'
 
 const PopUp1 = () => {
-  return (
-    
 
-    <div className="popup-container show">  
+    const [showPopup, setShowPopup] = useState(false);
+    const PopupCard = () => {
+        setShowPopup (!showPopup);
+    }
+
+
+      
+  return (
+    <div>  
+        {/* this button is only to show the pop up card */}
+        <button className='btn-green' onClick={PopupCard}>show card</button>
+
+        {showPopup && (
+    <div className="popup-container">  
           <div className='popup'>
             <h3>
                 Fare
             </h3>
-            <svg xmlns="http://www.w3.org/2000/svg" width="500" height="2" viewBox="0 0 920 2" fill="none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="2" viewBox="0 0 920 1" fill="none">
                 <path d="M0 0.640625L919.911 0.640708" stroke="#D7D7D7" stroke-width="1.25671"/>
             </svg>
 
@@ -25,24 +38,7 @@ const PopUp1 = () => {
                   </span>
               </div>
 
-              <div className='pop-flex'>
-                  <span className="list-name">
-                  Delivery Fee
-                  </span>
-                  <span className="list-price">
-                  #490.00
-                  </span>
-              </div>
-
-              <div className='pop-flex'>
-                  <span className="list-name">
-                  Delivery Fee
-                  </span>
-                  <span className="list-price">
-                  #490.00
-                  </span>
-              </div>
-
+              
               <div className='pop-flex'>
                   <span className="list-name">
                   Service Fee
@@ -62,18 +58,25 @@ const PopUp1 = () => {
             </div>
 
               <div className='pop-flex'>
-                  <span className="list-name">
+                  <span className="total-name">
                       Total
                   </span>
-                  <span className="list-price">
+                  <span className="total-price">
                       #520.00
                   </span>
+              </div>
+
+              <div className="btn-green">
+             <a href="http://">
+             Proceed
+                </a> 
               </div>
 
             </div>       
           </div>
         </div>
-     
+         )}
+        </div>
 
   )
 }
