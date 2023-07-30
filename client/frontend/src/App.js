94ce321cb6668f675a7cee1a84d8579f89d1bbcc
folarin <folarintaiye@gmail.com>
2023-07-30 06:15:42 +0100

    create a state managment for user authentication

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -5,7 +5,7 @@ import PasswordReset from "./components/Password reset/PasswordReset";
 
 import PasswordResetSuccess from "./components/password reset success/PasswordResetSuccess";
 
-// import UserSignup from "./components/user signup/UserSignup";
+import UserSignup from "./components/user signup/UserSignup";
 import Verification from "./components/verification/Verification";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Login />
         <PasswordReset />
         <PasswordResetSuccess />
-        {/* <UserSignup /> */}
+        <UserSignup />
         <Verification />
         <Loading />
       </header>
